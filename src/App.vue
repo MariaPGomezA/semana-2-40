ce5685165f60718173d5d65079558ad66b23ab52
leonpiedrahita <leondpr@gmail.com>
2020-12-05 15:28:17 -0500

    Menu y Banner en cli

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
+    <banner-principal> </banner-principal>
+
   </div>
 </template>
 
 <script>
 
+import BannerPrincipal from "./components/BannerPrincipal.vue";
+
 export default {
   name: 'App',
+  components: {
+  BannerPrincipal,
+  }
 }
 </script>
 
---
+++ src/components/MenuPrincipal.vue
@@ -0,0 +1,36 @@
+<template>
+    <nav class="navbar navbar-light nav-justified navbar-expand-lg sticky-top " style="background-color: #2F6BA1;">
+        <a class="navbar-brand ml-5" href="index.html">
+            <svg width="3em" height="3em" viewBox="0 0 16 16" class="bi bi-film" fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
+                    d="M0 1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V1zm4 0h8v6H4V1zm8 8H4v6h8V9zM1 1h2v2H1V1zm2 3H1v2h2V4zM1 7h2v2H1V7zm2 3H1v2h2v-2zm-2 3h2v2H1v-2zM15 1h-2v2h2V1zm-2 3h2v2h-2V4zm2 3h-2v2h2V7zm-2 3h2v2h-2v-2zm2 3h-2v2h2v-2z" />
+            </svg>
+
+        </a>
+        <div>
+            <h1 class=" ml-3 " href="">Semana-2-40</h1>
+        </div>
+        <div class="navbar-nav  mr-auto ml-auto h4">
+            <a class="nav-link mr-5 f" href="#">Inicio</a>
+            <a class="nav-link mr-5" href="#news">Noticias</a>
+            <a class="nav-link mr-5" href="#team">Miembros</a>
+
+        </div>
+        <div class="d-flex flex-row justify-content-center">
+            <a href="" class="btn btn-primary mr-3 ">Facebook</a>
+            <a href="" class="btn btn-danger">YouTube</a>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "MenuPrincipal",
+        
+    };
+</script>
+
+<style scoped>
+
+</style>
