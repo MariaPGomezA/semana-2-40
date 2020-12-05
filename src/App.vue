b3d2dbc0a83566de5a364897706cd053a7be9cd5
Felipebe19 <75180786+Felipebe19@users.noreply.github.com>
2020-12-05 18:32:58 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -32,35 +32,35 @@ data(){
         nombre: "Iván Rodríguez",
         descripcion: "Soy ingeniero en tele-comunicaciones y estudiante de especialización en gestión de proyectos de ingeniería en la Universidad Distrital en Bogotá. En mi tiempo libre me gusta leer.",
         rol: "Desarrollador backend",
-        image: "",
+        image: "./equipoimagenes/IR.jpg",
       },
       {
         codigo: 2,
         nombre: "León Piedrahita",
         descripcion: "Estudié bioingeniería en la Universidad de Antioquia. Vivo en Itagüí. Me gusta salir a caminar con mi perro.",
         rol: "Arquitecto de software",
-        image: "",
+        image: "./equipoimagenes/LP.jpg",
       },
       {
         codigo: 3,
         nombre: "María Paola Gómez",
         descripcion: "Estudié ingeniería química y especialización en dirección de producción y operaciones en la Universidad Nacional de Colombia, Sede Manizales. Pertenezco al coro de dicha Universidad.",
         rol: "Tester",
-        image: "",
+        image: "./equipoimagenes/MG.jpg",
       },
       {
         codigo: 4,
         nombre: "Andrés Felipe Bedoya",
         descripcion: "Estudio ingeniería industrial en la Universidad de Ibagué. Generalmente, vivo en Ibagué, pero actualmente estoy en Chaparral. Me gusta hacer ruta en bicicleta y senderismo.",
         rol: "Analista",
-        image: "",
+        image: "./equipoimagenes/FB.jpg",
       },
       {
         codigo: 5,
         nombre: "Felipe Garavito",
         descripcion: "Soy comunicador audiovisual de la Universidad de La Sabana. Vivo en Bogotá. Mi hobby es hacer vudeojuegos.",
         rol: "Desarrollador Frontend",
-        image: "",
+        image: "./equipoimagenes/FG.jpg",
       }
     ]
   }
