c6919131e1e0806be8669d0bb45c6e52ed3e43cd
IvanDarioRodriguezRubiano <ivdarodriguezrubiano@gmail.com>
2020-12-05 20:05:16 -0500

    Agregar archivos iss3

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <div class="container-fluid">
+      <section-api></section-api>
+    </div>
+  </div>
+</template>
+
+<script>
+import SectionApi from "./components/SectionApi.vue";
+
+export default {
+  name: "App",
+  components: {
+    SectionApi,
+  },
+};
+</script>
+
